1c653268cfa715f44376f0139075855ef02a6d82
dacho7 <davidtobarst@gmail.com>
2022-07-06 01:51:48 -0500

    Views implementation

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
 import express from "express";
 import cors from "cors";
 
-import { engine } from "express-handlebars";
-
 import { router } from "./routes/index";
+
 import db from "./models";
 
 const app = express();
@@ -11,10 +10,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
 app.use("/", router);
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./views");
 
 db.sequelize.sync().then(() => {
   app.listen(port, () => {
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import db from "../models";
 
-export const registerCredit = (req: Request, res: Response) => {
-  db.User.save({ username: "tobar", credits: 0 })
-    .then((result: any) => {
-      res.send(result);
-    })
-    .catch((err: any) => {
-      res.send(err);
-    });
+export const registerCredit = async (req: Request, res: Response) => {
+  const users: any = await db.User.findAll();
+  res.render("index");
+  //res.render("home");
+  //res.send(users);
+  // db.User.create({
+  //   username: "davidtoo",
+  //   credits: 10,
+  // });
 };
